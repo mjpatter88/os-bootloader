401d73b47fde5fe2d5f4b6294a36b15ad1698a03
Michael Patterson <mjpatter88@gmail.com>
2019-09-11 21:45:08 -0500

    Print hello to the screen.

--- boot.asm
+++ boot.asm
@@ -1,5 +1,28 @@
-loop:
-	jmp loop
+; https://en.wikipedia.org/wiki/BIOS_interrupt_call
+
+; Clear the screen by setting the video mode.
+mov al, 0x3
+mov ah, 0x00
+int 0x10
+int 0x10
+int 0x10
+
+; Setup to use the bios teletype interupt
+mov ah, 0x0e
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+; Loop forever
+jmp$
 
 times 510-($-$$) db 0
 
--- makefile
+++ makefile
@@ -2,4 +2,4 @@ boot.bin: boot.asm
 	nasm boot.asm -f bin -o boot.bin
 
 q-boot: boot.bin
-	qemu-system-i386 boot.bin
+	qemu-system-i386 -drive file=boot.bin,format=raw,index=0,media=disk
